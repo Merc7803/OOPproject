52ad397605ac4b878dcef7d8f2024ac13432aa0c
qkhanh711 <91941803+qkhanh711@users.noreply.github.com>
2023-04-16 20:13:42 +0700

    intelij

---
+++ LMP/out/production/LMP/Database/LM.sql
@@ -0,0 +1,99 @@
+create database library_managerment;
+use library_managerment;
+drop database library_managerment;
+-- phpMyAdmin SQL Dump
+-- version 3.4.5
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Jul 19, 2016 at 07:40 PM
+-- Server version: 5.5.16
+-- PHP Version: 5.4.0beta2-dev
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `librarian`
+--
+
+CREATE TABLE IF NOT EXISTS `librarian` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `address` varchar(200) NOT NULL,
+  `city` varchar(100) NOT NULL,
+  `contact` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+
+--
+-- Dumping data for table `librarian`
+--
+
+INSERT INTO `librarian` (`id`, `name`, `password`, `email`, `address`, `city`, `contact`) VALUES
+(1, 'khanh', 'nguyen', 'quockhanh@gmail.com', 'Ha Dong', 'Ha Noi', '0396115978'),
+(2, 'ha', 'nguyen', 'Nguyenha@gmail.com', 'Ha Dong', 'Ha Noi', '0365135581'),
+(3, 'hai', 'tram', 'tranhai@gmail.com', 'Ha Dong', 'Ha Noi', '0000000');
+
+CREATE TABLE IF NOT EXISTS `issuebooks` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `bookcallno` varchar(50) NOT NULL,
+  `studentid` int(11) NOT NULL,
+  `studentname` varchar(50) NOT NULL,
+  `studentcontact` varchar(20) NOT NULL,
+  `issueddate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
+
+--
+-- Dumping data for table `issuebooks`
+--
+
+INSERT INTO `issuebooks` (`id`, `bookcallno`, `studentid`, `studentname`, `studentcontact`, `issueddate`) VALUES
+(1, 'A@1', 23, 'abc', '932992932', '2016-07-19 18:43:16'),
+(2, 'A@1', 335, 'def', '95676565756', '2016-07-19 18:44:34'),
+(3, 'A@1', 87, 'ghi', '9329882382', '2016-07-19 18:46:12');
+
+CREATE TABLE IF NOT EXISTS `books` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `callno` varchar(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `author` varchar(100) NOT NULL,
+  `publisher` varchar(100) NOT NULL,
+  `quantity` int(10) NOT NULL,
+  `issued` int(10) NOT NULL,
+  `added_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `callno` (`callno`),
+  UNIQUE KEY `callno_2` (`callno`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `books`
+--
+
+INSERT INTO `books` (`id`, `callno`, `name`, `author`, `publisher`, `quantity`, `issued`, `added_date`) VALUES
+(1, 'A@4', 'C In Depth', 'Shrivastav', 'BPB', 2, 2, '2016-07-19 19:37:56'),
+(2, 'B@1', 'DBMS', 'Korth', 'Pearson', 3, 0, '2016-07-18 18:39:52'),
+(3, 'G@12', 'Let''s see', 'Yashwant Kanetkar', 'BPB', 10, 0, '2016-07-18 23:02:14');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
